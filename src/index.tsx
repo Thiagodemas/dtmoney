263f28952e3db0e2fcd3a694c716f9e9f06036cb
thiagodemas7 <thiago.demas@ignicaodigital.com.br>
2022-04-16 21:17:16 -0300

    Chama os valores cadastrados no API-FAKE

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,29 @@ createServer({
     transaction: Model
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de Website',
+          type: 'deposit',
+          category: 'dev',
+          amount: 6000,
+          created: new Date('2022-01-12 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 2000,
+          created: new Date('2022-03-01 08:00:00')
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
